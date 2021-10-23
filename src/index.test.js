5bee08918379c27d0f2f41432a7df54b2341ea01
danielcobo <danielcobo@users.noreply.github.com>
2021-10-24 00:27:43 +0200

    Jest test

---
+++ src/index.test.js
@@ -0,0 +1,42 @@
+const rmLeading = require('./index.js');
+const slash = '/';
+
+test('Removes leading substring', function () {
+  expect(rmLeading('/hello/world', slash)).toStrictEqual('hello/world');
+});
+
+test('Removes multiple leading substrings', function () {
+  expect(rmLeading('//hello/world', slash)).toStrictEqual('hello/world');
+});
+
+test('Removes all leading substrings', function () {
+  expect(rmLeading('///', slash)).toStrictEqual('');
+});
+
+test('Is OK with nothing to remove', function () {
+  expect(rmLeading('hello/world', slash)).toStrictEqual('hello/world');
+});
+
+test('Is OK with empty string', function () {
+  expect(rmLeading('', slash)).toStrictEqual('');
+});
+
+test('Use flag i to ignore case', function () {
+  expect(rmLeading('hello/world', 'Hello/', 'i')).toStrictEqual('world');
+});
+
+test('Mixed letters will not work', function () {
+  expect(rmLeading('hello/world', 'elHlo/', 'i')).toStrictEqual('hello/world');
+});
+
+test('Escape char will work', function () {
+  expect(rmLeading('hell\\o/world', 'hell\\o/')).toStrictEqual('world');
+});
+
+test('Throws for non-string str argument', function () {
+  expect(() => rmLeading(true, slash)).toThrow();
+});
+
+test('Throws for non-string substr argument', function () {
+  expect(() => rmLeading('hello/world', true)).toThrow();
+});
